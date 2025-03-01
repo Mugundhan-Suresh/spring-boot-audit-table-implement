433fecbffc105d028ab080b72871834656b58b5b
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-03-01 18:50:25 +0530

    Init spring boot project

---
+++ src/main/java/com/mugundhan/spring_audit_table/spring_audit_table/SpringAuditTableApplication.java
@@ -0,0 +1,13 @@
+package com.mugundhan.spring_audit_table.spring_audit_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuditTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuditTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugundhan/spring_audit_table/spring_audit_table/SpringAuditTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugundhan.spring_audit_table.spring_audit_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuditTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
